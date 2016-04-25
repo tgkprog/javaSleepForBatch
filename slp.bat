95f445fbc96cb48ec667ed714cad666b70815d03
Tushar Kapila <tushar.kapila@sel2in.com>
2016-04-25 23:38:59 +0530

    version 1 works on windows 8

--- slp.bat
+++ slp.bat
@@ -1,3 +1,4 @@
 @echo off
 echo hi! -tgkprog@gmail.com http://sel2in.com
-java -cp "c:\apps\utls\slp.jar" sel2in.Sleep %*
+rem the / works on windows too as its passed to java which parses it out correctly for windows
+java -cp "c:/apps/utls/slp.jar;./slp.jar" sel2in.Sleep %*
